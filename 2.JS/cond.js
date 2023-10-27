40fbfa7af486c40917ff492e1c5c9e36cc3307d0
Dukov <rhtkdals135@gmail.com>
2023-10-27 15:46:05 +0900

    cal_feb

--- 2.JS/cond.js
+++ 2.JS/cond.js
@@ -1,5 +1,5 @@
-let score = 50;
-
+let score = "65";
+console.log("-----if-----");
 if (score >= 90)
     console.log("A");
 else if (score >= 80)
@@ -10,3 +10,20 @@ else if (score >= 60)
     console.log("D");
 else
     console.log("You Failed!");
+
+console.log("-----switch-----");
+score = 80;
+switch(score) {
+    case 90:
+        console.log("A");
+        break;
+    case 80:
+        console.log("B");
+        break;
+    case 70:
+        console.log("C");
+        break;
+    case 60:
+        console.log("D");
+        break;
+}
---
+++ 2.JS/loop.js
@@ -0,0 +1,28 @@
+// for (i = 0; i < 5; i++) {
+//     console.log("Hello");
+//     for (j = 0; j < 5; j++)
+//         console.log("j = " + j);
+// }
+
+// for (let i = 1; i < 10; i++) {
+//     console.log(i + "단");
+//     for (let j = 1; j < 10; j++) {
+//         console.log(i + "x" + j + "=" + (i * j));
+//     }
+//     console.log("");
+// }
+
+let n = 0;
+// while (n < 10) {
+//     console.log(n);
+//     n += 1;
+// }
+
+n = 0;
+do {
+    console.log(n);
+    n += 1;
+} while (n < 3) {
+    console.log(n);
+    n += 1;
+}
