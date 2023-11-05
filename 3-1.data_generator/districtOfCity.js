9d8fea81b6127d8dd82364920879244a634a3d63
rhtkdals135 <rhtkdals135@gmail.com>
2023-11-05 22:09:30 +0900

    user generator done

---
+++ 3-1.data_generator/districtOfCity.js
@@ -0,0 +1,44 @@
+import fs from "fs";
+
+export function districtOfCity(city) {
+    const districtFile = fs.readFileSync("./userAddressElement.json", "utf8");
+    const districtData = JSON.parse(districtFile);
+    switch (city) {
+        case "서울":
+            return (districtData.seoulDistrict[Math.floor(Math.random() * 25)]);
+        case "부산":
+            return (districtData.busanDistrict[Math.floor(Math.random() * 16)]);
+        case "인천":
+            return (districtData.incheonDistrict[Math.floor(Math.random() * 10)]);
+        case "대구":
+            return (districtData.daeguDistrict[Math.floor(Math.random() * 9)]);
+        case "대전":
+            return (districtData.daejeonDistrict[Math.floor(Math.random() * 5)]);
+        case "광주":
+            return (districtData.gwangjuDistrict[Math.floor(Math.random() * 5)]);
+        case "수원":
+            return (districtData.suwonDistrict[Math.floor(Math.random() * 4)]);
+        case "울산":
+            return (districtData.ulsanDistrict[Math.floor(Math.random() * 5)]);
+        case "용인":
+            return (districtData.yonginDistrict[Math.floor(Math.random() * 3)]);
+        case "고양":
+            return (districtData.goyangDistrict[Math.floor(Math.random() * 3)]);
+        case "창원":
+            return (districtData.changwonDistrict[Math.floor(Math.random() * 5)]);
+        case "성남":
+            return (districtData.sungnamDistrict[Math.floor(Math.random() * 3)]);
+        case "청주":
+            return (districtData.chungjuDistrict[Math.floor(Math.random() * 4)]);
+        case "부천":
+            return (districtData.bucheonDistrict[Math.floor(Math.random() * 3)]);
+        case "천안":
+            return (districtData.cheonahnDistrict[Math.floor(Math.random() * 2)]);
+        case "전주":
+            return (districtData.jeonjuDistrict[Math.floor(Math.random() * 2)]);
+        case "안산":
+            return (districtData.ahnsanDistrict[Math.floor(Math.random() * 2)]);
+        case "안양":
+            return (districtData.ahnyangDistrict[Math.floor(Math.random() * 2)]);
+    }
+}
--- 3-1.data_generator/userGenerator.js
+++ 3-1.data_generator/userGenerator.js
@@ -1,7 +1,9 @@
 import fs from "fs";
 import { v4 as uuidv4 } from "uuid";
+import { dateOfMonth } from "./dateOfMonth.js";
+import { districtOfCity } from "./districtOfCity.js";
 
-class userData {
+export class userData {
     userIdGenerate() {
         this.userId = uuidv4();
     }
@@ -11,7 +13,7 @@ class userData {
     }
 
     userNameGenerate() {
-        const userNameFile = fs.readFileSync("./userNameElement.json", {encoding: "utf8"});
+        const userNameFile = fs.readFileSync("./userNameElement.json", "utf8");
         const userNameData = JSON.parse(userNameFile);
         const userLastName = userNameData.lastName[Math.floor(Math.random() * 50)];
         let userFirstName;
@@ -24,25 +26,64 @@ class userData {
         this.userName = userLastName + userFirstName;
     }
 
-    // userAgeGenerate() {
+    userAgeGenerate() {
+        this.userAge = Math.floor(Math.random() * 87) + 14;
+    }
 
-    // }
+    userBirthdateGenerate() {
+        const today = new Date().toLocaleDateString("ko-KR", {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit"
+        });
+        const todayYear = parseInt(today.slice(0, 4));
+        const todayMonth = parseInt(today.slice(6, 8));
+        const todayDay = parseInt(today.slice(10, 12));
+        this.userBirthYear = Math.random() < 0.5 ? todayYear - this.userAge : todayYear - this.userAge - 1;
+        if (this.userBirthYear == todayYear - this.userAge) {
+            this.userBirthMonth = Math.floor(Math.random() * todayMonth) + 1;
+            const dayRange = dateOfMonth(this.userBirthMonth);
+            if (this.userBirthMonth == todayMonth)
+                this.userBirthDay = Math.floor(Math.random() * todayDay) + 1;
+            else
+                this.userBirthDay = Math.floor(Math.random() * dayRange) + 1;
+        }
+        else {
+            this.userBirthMonth = todayMonth + Math.floor(Math.random() * (13 - todayMonth));
+            const dayRange = dateOfMonth(this.userBirthMonth);
+            if (this.userBirthMonth == todayMonth)
+                this.userBirthDay = todayDay + Math.floor(Math.random() * (dayRange - todayDay)) + 1;
+            else
+                this.userBirthDay = Math.floor(Math.random() * dayRange) + 1;
+        }
+        this.userBirthDate = this.userBirthYear + "-" + String(this.userBirthMonth).padStart(2, "0") + "-" + String(this.userBirthDay).padStart(2, "0");
+    }
 
-    // userBirthdateGenerate() {
-
-    // }
-
-    // userAddressGenerate() {
-
-    // }
-
-    // toString() {
-
-    // }
+    userAddressGenerate() {
+        const cityNameFile = fs.readFileSync("./userAddressElement.json", "utf8");
+        const cityNameData = JSON.parse(cityNameFile);
+        this.userAddressCity = cityNameData.city[Math.floor(Math.random() * 18)];
+        this.userAddressDistrict = districtOfCity(this.userAddressCity);
+        this.userAddressRoad = (Math.floor(Math.random() * 100) + 1) + (Math.floor(Math.random()) < 0.5 ? "길" : "로");
+        this.userAddressNum = Math.floor(Math.random() * 100) + 1;
+        this.userAddress = this.userAddressCity + " " + this.userAddressDistrict + " " + this.userAddressRoad + " " + this.userAddressNum;
+    }
 }
 
-const user1 = new userData();
-user1.userGenderGenerate();
-user1.userNameGenerate();
-console.log(user1.userGender);
-console.log(user1.userName);
+const userDataArr = [];
+
+for (let i = 0; i < 1000; i++) {
+    const userInstance = new userData();
+    userInstance.userIdGenerate();
+    userInstance.userGenderGenerate();
+    userInstance.userNameGenerate();
+    userInstance.userAgeGenerate();
+    userInstance.userBirthdateGenerate();
+    userInstance.userAddressGenerate();
+    userDataArr.push(userInstance);
+}
+
+const userDataCsv = userDataArr.map(user => `${user.userId},${user.userName},${user.userGender},${user.userAge},${user.userBirthDate},${user.userAddress}`).join("\n");
+const userDataCsvHeader = "Id,Name,Gender,Age,Birthdate,Address\n";
+fs.writeFileSync("user.csv", userDataCsvHeader, "utf8");
+fs.appendFileSync("user.csv", userDataCsv, "utf8");
