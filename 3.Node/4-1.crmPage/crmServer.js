cda52899767fb901f8a7c651a25f014d58ac9e2b
Dukov <rhtkdals135@gmail.com>
2023-11-21 15:44:08 +0900

    to crmpage paging

--- 3.Node/4-1.crmPage/crmServer.js
+++ 3.Node/4-1.crmPage/crmServer.js
@@ -6,6 +6,19 @@ import csvtojson from "csvtojson";
 const app = express();
 const port = 3000;
 const __dirname = path.resolve();
+const userDataJson = await new Promise((resolve) => {
+    const jsonArr = [];
+
+    csvtojson()
+        .fromFile(path.join(__dirname, "public", "static", "user.csv"), "utf8")
+        .subscribe((jsonObj, lineNumber) => {
+            if (lineNumber >= 0 && lineNumber < 200) 
+                jsonArr.push(jsonObj);
+        })
+        .on("done", () => {
+            resolve(jsonArr);
+        })
+});
 
 nunjucks.configure("views", {
     autoescape: true,
@@ -16,21 +29,27 @@ app.use("/static", express.static(path.join(__dirname, "public", "static")));
 
 app.set("view engine", "html");
 
-app.get("/", async (req, resp) => {
-    const userDataJson = await new Promise((resolve) => {
-        const jsonArr = [];
+app.use((req, resp, next) => {
+    const start = Date.now();
 
-        csvtojson()
-            .fromFile(path.join(__dirname, "public", "static", "user.csv"), "utf8")
-            .subscribe((jsonObj, lineNumber) => {
-                if (lineNumber >= 0 && lineNumber < 20)
-                    jsonArr.push(jsonObj);
-            })
-            .on("done", () => {
-                resolve(jsonArr);
-            })
+    resp.on("finish", () => {
+        const end = Date.now();
+        const duration = end - start;
+
+        console.log(`request: ${req.path}, response: ${duration}`);
     });
-    resp.render("index", { userDataJson });
+
+    next();
+});
+
+app.get("/", (req, resp) => {
+    const itemsPerPage = 15;
+    const page = req.query.page || 1;
+    const startIndex = itemsPerPage * (page - 1);
+    const endIndex = startIndex + itemsPerPage;
+    const totalPages = Math.ceil(userDataJson.length / itemsPerPage);
+    const slicedData = userDataJson.slice(startIndex, endIndex);
+    resp.render("index", { userDataJson: slicedData, title: "Hello", totalPage: parseInt(totalPages), currentPage: parseInt(page)});
 });
 
 app.listen(port, () => {
