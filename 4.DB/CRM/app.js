09838da13ad096a7e90d5c543685db855a7f959e
rhtkdals135 <rhtkdals135@gmail.com>
2023-12-12 00:55:26 +0900

    store-info done

--- 4.DB/CRM/app.js
+++ 4.DB/CRM/app.js
@@ -1,5 +1,4 @@
 import express from "express";
-import morgan from "morgan";
 import path from "path";
 import { initDatabase } from "./src/database/initDB.js";
 import { router as userRouter } from "./src/routes/userRoutes.js";
--- 4.DB/CRM/src/controllers/storeController.js
+++ 4.DB/CRM/src/controllers/storeController.js
@@ -1,4 +1,5 @@
 import { Store } from "../utils/store.js";
+import { db } from "../database/initDB.js";
 
 const storeList = new Store();
 
@@ -27,6 +28,58 @@ export const storeListRenderer = async (req, resp) => {
     });
 };
 
-export const storeInfoRenderer = (req, resp) => {
-    resp.render("storeInfo");
+export const storeInfoRenderer = async (req, resp) => {
+    const dataArr = [];
+    try {
+        const queries = [{ 
+            sql:`SELECT Name, Type, Address 
+                FROM "store" 
+                WHERE Id=?;`,
+            single: true }, 
+            { 
+            sql:`SELECT strftime('%Y-%m', o.OrderAt) AS Month, SUM(i.UnitPrice) AS Revenue, COUNT(*) AS Count
+                FROM 'order' AS o
+                JOIN 'orderItem' AS oi
+                ON o.Id = oi.OrderId
+                JOIN 'item' AS i
+                ON i.Id = oi.ItemId
+                WHERE o.StoreId=?
+                GROUP BY Month;`,
+            single: false },
+            {
+            sql:`SELECT u.Id AS UserId, u.Name AS Name, COUNT(*) AS Frequency
+                FROM 'user' AS u
+                JOIN 'order' AS o
+                ON u.Id = o.UserId
+                WHERE o.StoreId=?
+                GROUP BY u.Name
+                ORDER BY Frequency DESC
+                LIMIT 10;`,
+            single: false }
+        ];
+
+        for (const query of queries) { // 함수화, data 관련 폴더로
+            const data = await new Promise((resolve, reject) => {
+                const method = query.single ? "get" : "all";
+
+                db[method](query.sql, req.params.storeId, (err, row) => {
+                    if (err)
+                        reject(err);
+                    else
+                        resolve(row);
+                });
+            });
+            dataArr.push(data);
+        }
+    } catch (err) {
+        console.error(err.message);
+        resp.status(500).json({ message: "Internal server error" });
+        return;
+    }
+    
+    resp.render("storeInfo", {
+        storeData: dataArr[0],
+        revenueData: dataArr[1],
+        regularData: dataArr[2]
+    });
 };
